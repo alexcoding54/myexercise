03186e6b22bc02256cf12dc5df9c6e7b587c55d1
alex <140233794+alexcoding54@users.noreply.github.com>
2023-08-19 14:53:06 +0800

    Improve the previous bubblesort algorithm
    It's a more effective bubblesort than the one I've uploaded previously.
    It's just only 3 deep to implement the sorting func, the previous one is 4 deep.

---
+++ algorithm/bubblesort_plus.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define for_each(i, x) \
+    for (int i=0; i<x; i++)
+
+void swap(int *m,int *n)
+{
+    int t = *m;
+    *m = *n;
+    *n = t;
+}
+
+void bubble_sort(int *arr, int n)
+{
+    int m = 0;
+
+    while (m!=n) {
+        if (arr[m] <= arr[m+1]) {
+            m++;
+        } else if (arr[m] > arr[m+1]) {
+            swap(&arr[m], &arr[m+1]);
+            m = 0;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+    
+    int arr[n];
+
+    for_each(i, n) {
+        scanf("%d", &arr[i]);
+    }
+
+    bubble_sort(arr, n);
+
+    for_each(i, n) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
